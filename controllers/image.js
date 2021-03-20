1f4b85810f6c3b3e0a4c99f8f05f7f4d9b294a78
peter <peter@grenadine.co>
2021-03-20 16:34:28 -0400

    added clarifai to image controllers

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+    apiKey: '5c3b2d28107b422bb8a6140e7d7c70e3'
+});
+
+const handleImageApiCall = (req, res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(response => {
+            return res.json(response);
+        })
+        .catch(err => {
+            console.log('error: ', err);
+            res.status(400).json('failed to get image');
+        });
+};
 
 const handleImage = (req, res, pgdb)=> {
     const {id} = req.body;
@@ -17,4 +33,4 @@ const handleImage = (req, res, pgdb)=> {
     .catch(err => res.status(400).json('Error getting user'));
 };
 
-export default handleImage;
+export {handleImage, handleImageApiCall};
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import bcrypt from "bcrypt";
 import handleRegister from "./controllers/register.js";
 import handleSignin from "./controllers/signin.js";
 import handleProfile from "./controllers/profile.js";
-import handleImage from "./controllers/image.js";
+import {handleImage, handleImageApiCall} from "./controllers/image.js";
 
 const app = express();
 app.use(express.json());
@@ -32,12 +32,10 @@ app.get('/', (req, res)=> {
 });
 
 app.post('/signin', (req, res)=> { handleSignin(req, res, pgdb, bcrypt) });
-
 app.post('/register', (req, res)=> { handleRegister(req, res, pgdb, bcrypt) });
-
 app.get('/profile/:id', (req, res)=> { handleProfile(req, res, pgdb) });
-
 app.put('/image', (req, res)=> { handleImage(req, res, pgdb) });
+app.post('/imageurl', (req, res)=> { handleImageApiCall(req, res) });
 
 app.listen(3000, ()=> {
     console.log('app is running!!!');
