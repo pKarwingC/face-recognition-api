f15378a1f94d0559df5259ddc2c67dfd4d02c989
peter <peter@grenadine.co>
2021-03-21 14:44:27 -0400

    update db host

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 const pgdb = knex({
     client: 'pg',
     connection: {
-        host : '127.0.0.1',
+        host : 'postgresql-rigid-66397',
         user : '',
         password : '',
         database : 'face-recognition'
